00dc0fc7a464723dfca04f94f220052520cd0ef6
sinatra27 <147604618+sinatra27@users.noreply.github.com>
2024-02-13 03:18:06 -0500

    Add feature to interface then update style and logic

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // Initialize a container for the square divs
 const container = document.querySelector('.container');
 
-// temp size
-let size = 16;
+// Prompt the user to select grid size
+    // let size = Number(window.prompt('Please type a number (max 100) for grid size...'));
 
-// Use For loop to create size x size grid of square divs, add square divs under the container div
-for (let i = 0; i < size * size; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+// Function to create grid while passing in the input size by the user
+function createGrid(size) {
+    // Clear grid when grid size is changed
+    container.replaceChildren();
+    // Use For loops to create size x size grid of square divs, add divs under the container
+    for (let i = 0; i < size; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            column.appendChild(square);
+        }
+        container.appendChild(column);
+    }
 }
 
-// Declare variables for RGB values
+// Declare variables to use with random RGB values
     // let randomColor;
 let r, g, b;
 
@@ -30,3 +40,15 @@ changeColor.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
 
+
+// let sizeRange = document.querySelector("#rangeSize");
+// let sizeValue = Number(sizeRange.value);
+
+createGrid(16);                     // Grid size default set at 16 x 16
+
+
+
+
+
+// add user options: grid size and regen, bg color, pen color, grid on/off
+// optional: darkening effect

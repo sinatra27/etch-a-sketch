ef5646cbef0c25dee69295beb3dbee414c9b60c7
sinatra27 <147604618+sinatra27@users.noreply.github.com>
2024-02-14 15:49:55 -0500

    Add more features to interface and update style and logic

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Initialize a container for the square divs
 const container = document.querySelector('.container');
 
-// Prompt the user to select grid size
-    // let size = Number(window.prompt('Please type a number (max 100) for grid size...'));
-
 // Function to create grid while passing in the input size by the user
 function createGrid(size) {
     // Clear grid when grid size is changed
@@ -14,6 +11,7 @@ function createGrid(size) {
         column.classList.add('column');
         for (let j = 0; j < size; j++) {
             const square = document.createElement('div');
+            if (!gridlinesOn) square.style.border = 'none';
             square.classList.add('square');
             column.appendChild(square);
         }
@@ -21,34 +19,70 @@ function createGrid(size) {
     }
 }
 
-// Declare variables to use with random RGB values
-    // let randomColor;
-let r, g, b;
+// Update background color 
+function bgColorUpdate(bgColor) {
+    container.style.backgroundColor = bgColor;
+}
 
-// Change color of square divs
+// Select pen color
+const penColor = document.getElementById('penColorSelect');
+
+// Rainbow (random RGB pen color) mode
+let r, g, b;                        // Declare variables for random RGB values
+let rainbowMode = false;
+function rainbowToggle() {
+    rainbowMode = !rainbowMode;
+}
+
+// Gridlines toggle on/off
+let gridlinesOn = true;
+function gridlinesToggle() {
+    gridlinesOn = !gridlinesOn;
+    if (!gridlinesOn) {
+        Array.from(document.querySelectorAll('.square')).forEach((el) => el.style.border = 'none');
+    }
+    else {
+        Array.from(document.querySelectorAll('.square')).forEach((el) => el.style.border = '1px solid #000000');
+    }     
+}
+
+// Eraser toggle on/off
+const erasorColor = document.getElementById('bgColorSelect');
+let eraserOn = false;
+function eraserToggle() {
+    eraserOn = !eraserOn;    
+}
+
+// Clear grid while keeping current settings
+function clearGrid() {
+    Array.from(document.querySelectorAll('.square')).forEach((el) => el.style.backgroundColor = null);
+}
+
+// Declare variable for mouse down and up, set initital value to false
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false); 
+
+// Change pen color for squares
 let changeColor = document.querySelector('div');
-changeColor.addEventListener('mouseover', (e) => {
-        // e.target.classList.replace('square', 'color');
-        // randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        // e.target.style.backgroundColor = '#' + randomColor;
-    // Use conditional to select colors based on user's choice
+changeColor.addEventListener('mouseover', sketch);
+changeColor.addEventListener('mousedown', sketch);
 
-    // Randomize RGB values to change square colors
-    r = Math.floor(Math.random() * 255);
-    g = Math.floor(Math.random() * 255);
-    b = Math.floor(Math.random() * 255);
-    e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
+function sketch(e) {
+        // Use conditional for when mouse is not clicked
+        if (e.type === 'mouseover' && !mouseDown) return;
+        // Use conditional to select colors or erasor based on user's choice
+        if (eraserOn) e.target.style.backgroundColor = erasorColor.value;
+        else if (rainbowMode) {
+            // Randomize RGB values for square colors. RGB color range is 0-255
+            r = Math.floor(Math.random() * 255);        
+            g = Math.floor(Math.random() * 255);
+            b = Math.floor(Math.random() * 255);
+            e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
+        else e.target.style.backgroundColor = penColor.value;
+};
 
-
-// let sizeRange = document.querySelector("#rangeSize");
-// let sizeValue = Number(sizeRange.value);
-
-createGrid(16);                     // Grid size default set at 16 x 16
-
-
-
+createGrid(16);                     // Initial grid size set at 16 x 16
 
 
-// add user options: grid size and regen, bg color, pen color, grid on/off
-// optional: darkening effect
